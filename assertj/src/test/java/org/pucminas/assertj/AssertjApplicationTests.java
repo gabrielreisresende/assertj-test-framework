cd06e40f201121733ec3ef0be0a387defe277af6
Gabriel Resende <gabriellrr2005@gmail.com>
2025-10-07 08:23:25 -0300

    feat: project created

---
+++ assertj/src/test/java/org/pucminas/assertj/AssertjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pucminas.assertj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssertjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
